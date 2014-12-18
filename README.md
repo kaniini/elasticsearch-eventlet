6eafdfc5e81829c60ffb05fa0db2329ffaecadd6
William Pitcock <nenolod@dereferenced.org>
2014-12-18 12:02:46 -0600

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ elasticsearch-eventlet
 ======================
 
 An eventlet-aware elasticsearch client.
+
+Right now it only supports indexing (in bulk mode) and counting, which are the two things
+I care about on the eventlet side.  Pull requests are welcome to add the other features.
+
+Usage:
+
+```python
+import eventlet
+from elasticsearch_eventlet import ElasticSearch
+
+es = ElasticSearch('http://127.0.0.1:9200/')
+
+def worker():
+    docs = [{'number': i} for i in xrange(0, 1024)]
+    es.bulk_index('numbers', 'number', docs)
+
+eventlet.spawn(worker)
+eventlet.join_all()
+```
+
+Of course this is a silly example, but unlike the default ElasticSearch client, it won't go
+mental spawning tons of connections when there is lag which is the real point.
+
---
+++ elasticsearch_eventlet.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2014, William Pitcock <nenolod@dereferenced.org>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+try:
+    import simplejson as json
+except:
+    import json
+
+import eventlet
+import erequests
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ElasticSearch(object):
+    def __init__(self, size=10, base_url='http://127.0.0.1:9200/'):
+        self.pool = eventlet.GreenPool(size)
+        self.base_url = base_url
+        self.session = erequests.Session()
+        if self.base_url[-1] != '/':
+            self.base_url += '/'
+
+    def map(self, requests):
+        def submit(r):
+            try:
+                return r.send()
+            except Exception as e:
+                return e
+        jobs = [self.pool.spawn(submit, r) for r in requests]
+        for j in jobs:
+            yield j.wait()
+
+    def map_one(self, request):
+        return list(self.map([request]))[0]
+
+    def count(self, index, query=None):
+        method = 'POST' if query else 'GET'
+        url = self.base_url + index + '/_count'
+
+        asr = erequests.AsyncRequest(method, url, self.session)
+        if query:
+            asr.prepare(data=json.dumps(query))
+        return self.map_one(asr)
+
+    def bulk_index(self, index, doc_type, docs):
+        chunks = []
+        for doc in docs:
+            action = {'index': {'_index': index, '_type': doc_type}}
+
+            if doc.get(id_field) is not None:
+                action['index']['_id'] = doc[id_field]
+
+            if doc.get(parent_field) is not None:
+                action['index']['_parent'] = doc.pop(parent_field)
+
+            chunks.append(json.dumps(action))
+            chunks.append(json.dumps(doc))
+
+        payload = '\n'.join(chunks) + '\n'
+        url = self.base_url + index + '/' + doc_type + '/_bulk'
+        asr = erequests.AsyncRequest('POST', url, self.session)
+        asr.prepare(data=payload)
+        return self.map_one(asr)
