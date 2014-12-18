f7d88e7df3a213be75fcb11bcde8a3ed77050a7c
William Pitcock <nenolod@dereferenced.org>
2014-12-18 12:21:24 -0600

    add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+setup (
+    name='elasticsearch_eventlet',
+    version='0',
+    packages=['elasticsearch_eventlet'],
+    description='elasticsearch library which uses eventlet primitives',
+    author='William Pitcock',
+    author_email='nenolod@dereferenced.org',
+    url='http://kaniini.dereferenced.org/elasticsearch-eventlet/'
+)
